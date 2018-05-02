2aa2ece9246e8a39be42d1a225362337c09803c7
Pedro Romano <pedro.romano@holidayextras.com>
2018-05-02 13:55:17 +0100

    Tell fortune.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+const childProcess = require('child_process')
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send(childProcess.execSync('fortune')))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Fortune teller listening on port 3000!'))
